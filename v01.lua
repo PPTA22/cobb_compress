8669b11f7440e2fe4f9ba9e75bd5c2f097f0d322
TPu7BL <40493117+TPu7BL@users.noreply.github.com>
2020-04-29 23:08:26 +0800

    Update v01.lua
    add future planning

--- v01.lua
+++ v01.lua
@@ -1,20 +1,37 @@
+-- clear all the slot, at the begining of the looping, avoid bug
+-- check slot is empty before suck and 64 cobb,
+-- since if the block is not empty and keep suck,
+-- it will put in next box which cause the program crush
+
 while (true) do
+    boo, data = turtle.inspect()
+    if data.name == "minecraft:chest"
+    then 
+        turtle.select(1)
+        -- if turtle.getItemDetail()
+        turtle.suck()
+        turtle.select(2)
+        turtle.suck()
+        turtle.select(3)
+        turtle.suck()
 
-turtle.select(1)
-turtle.suck()
-turtle.suck()
-turtle.suck()
+        turtle.select(5)
+        turtle.suck()
+        turtle.select(6)
+        turtle.suck()
+        turtle.select(7)
+        turtle.suck()
 
-turtle.select(5)
-turtle.suck()
-turtle.suck()
-turtle.suck()
+        turtle.select(9)
+        turtle.suck()
+        turtle.select(10)
+        turtle.suck()
+        turtle.select(11)
+        turtle.suck()
 
-turtle.select(9)
-turtle.suck()
-turtle.suck()
-turtle.suck()
-
-turtle.craft()
-turtle.dropUp()
+        turtle.craft()
+        turtle.dropUp()
+    else
+        error ("[x] plz put a chest with compress itme in front of the turtle")
+    end
 end 
